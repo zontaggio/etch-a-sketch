c82fa58592956bae007e2ba7d6da1b02b8d08f51
Zonta <giordano.zonta@gmail.com>
2023-12-13 23:32:13 -0300

    Created the MVP for HTML, CSS and JS.

---
+++ script.js
@@ -0,0 +1,61 @@
+var slider = document.getElementById("myRange");
+var drawContainer = document.getElementById("draw-container");
+var matrixSize = document.getElementById("matrix-size");
+let isLeftMouseButtonDown = false;
+
+// Create the grid
+function createGrid(value) {
+    drawContainer.innerHTML = ''; // Clean the actual grid
+
+    for (let i = 1; i <= value; i++) {
+        const newLine = document.createElement("div");
+        newLine.classList.add('line');
+        drawContainer.appendChild(newLine);
+
+        for (let j = 1; j <= value; j++) {
+            const newItem = document.createElement("div");
+            newItem.classList.add('item');
+            newLine.appendChild(newItem);
+        }    
+    }
+
+    addMouseOverEventToItems();
+}
+
+// Mouseover to paint it with black color and unpaint it if not clicked
+function addMouseOverEventToItems() {
+    document.querySelectorAll('div.item').forEach(item => {
+        item.addEventListener('mouseover', function() {
+            if (isLeftMouseButtonDown) {
+                item.style.backgroundColor = "black";
+            } else {
+                item.style.backgroundColor = "";
+            }
+        });
+    });
+}
+
+// Listeners to track the mouse left button click
+document.addEventListener('mousedown', function(event) {
+    if (event.button === 0) {
+        isLeftMouseButtonDown = true;
+    }
+});
+
+document.addEventListener('mouseup', function(event) {
+    if (event.button === 0) {
+        isLeftMouseButtonDown = false;
+    }
+});
+
+// Create a grid based on the slider value
+slider.oninput = function() {
+    createGrid(slider.value);
+    let sliderLabel = slider.value + "x" + slider.value;
+    matrixSize.innerHTML = sliderLabel;
+}
+
+// Create the initial grid
+createGrid(slider.value);
+matrixSize.innerHTML = '16x16'
+x
